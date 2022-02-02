1383c4a493033f478b3f827ee8b57ebf1f5a5c8d
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2022-02-02 14:12:36 -0300

    feat: adding react wrapper lib

---
+++ react/src/components/stencil-generated/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'aw-components';
+
+import { defineCustomElements } from 'aw-components/loader';
+
+defineCustomElements();
+export const AwButton = /*@__PURE__*/createReactComponent<JSX.AwButton, HTMLAwButtonElement>('aw-button');
+export const AwIconArrow = /*@__PURE__*/createReactComponent<JSX.AwIconArrow, HTMLAwIconArrowElement>('aw-icon-arrow');
+export const AwIconSearch = /*@__PURE__*/createReactComponent<JSX.AwIconSearch, HTMLAwIconSearchElement>('aw-icon-search');
+export const AwvComponentGroup = /*@__PURE__*/createReactComponent<JSX.AwvComponentGroup, HTMLAwvComponentGroupElement>('awv-component-group');
